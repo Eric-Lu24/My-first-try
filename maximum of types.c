38cbafc26f319cc83c247e353d1592a4d3e92141
Eric.Lu <132185686+Eric-Lu24@users.noreply.github.com>
2026-06-30 23:24:40 +0800

    Add files via upload

---
+++ maximum of types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int a=0;
+	while(a>=0){
+		a++;
+	}
+	printf("int类型的最大数值为%d\n",a-1);
+	unsigned int b=1;
+	while(b>0){
+		b++;
+	}
+	printf("unsigned int类型的最大数值为%u\n",b-1);
+//32bits 
+}
