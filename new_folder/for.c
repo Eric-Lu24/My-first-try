d123f258aae52f5f7e54364e858037809f8475d8
Eric.Lu <132185686+Eric-Lu24@users.noreply.github.com>
2026-07-06 22:46:01 +0800

    Rename for.c to new_folder/for.c

--- new_folder/for.c
+++ new_folder/for.c
@@ -2,13 +2,13 @@
 
 int main(){
 	int n;
-	printf("请输入阶乘正整数:\n");
+	printf("璇疯緭鍏ラ樁涔樻鏁存暟:\n");
 	scanf("%d",&n);
     int i=1;
 	int result=1;	
 	for(i=1;i<=n;i++){
 		result *= i;
 	}
-	printf("%d!的结果为:%d",n,result);
+	printf("%d!鐨勭粨鏋滀负:%d",n,result);
 	return 0;
 }
